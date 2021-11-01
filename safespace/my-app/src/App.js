916ae8ee46a05e77c4d1aa2122d56b6be468a217
Vanessa <vanessa@purewatercraft.com>
2021-11-01 09:37:05 -0700

    init the web page layout

--- safespace/my-app/src/App.js
+++ safespace/my-app/src/App.js
@@ -2,24 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // TODO: implement other pages to render
+  // const renderOnboardingPage = (routerProps) => {
+  //   return <OnboardingPage {... routerProps} props={props}/>
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Header /> */}
+      <div className='mainContent'>
+        <Switch>
+          {/* TODO: Add the other pages */}
+          {/* <Route exact path="/onboarding" render={renderOnboardingPage} />
+                <Redirect to="/onboarding" /> */}
+        </Switch>
+      </div>
+      {/* <Footer /> */}
     </div>
-  );
+  )
 }
 
 export default App;
